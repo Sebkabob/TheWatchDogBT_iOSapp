3e97997baa4950d41d476c005ba606dc8b0fb20e
Sebkabob <Sebforenza@gmail.com>
2025-10-30 17:20:35 -0700

    Initial Commit

---
+++ BluetoothTesting/BluetoothTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTestingApp.swift
+//  BluetoothTesting
+//
+//  Created by Sebastian Forenza on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothTestingTests/BluetoothTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTestingTests.swift
+//  BluetoothTestingTests
+//
+//  Created by Sebastian Forenza on 10/30/25.
+//
+
+import Testing
+@testable import BluetoothTesting
+
+struct BluetoothTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
